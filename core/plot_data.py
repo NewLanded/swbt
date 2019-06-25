b6d31aa0c35182b3a741f566aa5b9de46d3d2c89
NewLanded <l1141041@163.com>
2019-06-25 15:21:53 +0800

    nothing

--- core/plot_data.py
+++ core/plot_data.py
@@ -64,6 +64,53 @@ def plot_data(point_data, bs_data, manual_plot_data):
     plt.show()
 
 
+def plot_data_2(ts_code, point_data, point_data_part):
+    """记录matplotlib使用过程中x轴刻度设置出的一些问题的解决方法
+    1. x轴刻度为日期且不连续
+    2. 设置刻度间隔
+    """
+    sns.set()
+    sns.set_palette(sns.color_palette('dark'))
+
+    point_data_part['xticks_int_index'] = [i for i in range(len(point_data_part["trade_date"]))]
+    xticks_index_map = point_data_part["trade_date"].apply(lambda x: x.strftime("%Y-%m-%d"))
+
+    def format_date(x, pos=None):
+        if x < 0 or x > len(xticks_index_map) - 1:
+            return ''
+        return xticks_index_map.iloc[int(x)]
+
+    fig = plt.figure(figsize=(16, 9))
+
+    ax1 = plt.axes([0, 0, 0.7, 1])
+    ax1.plot(point_data["trade_date"], point_data["close"], ls="-", lw=1, color='k')
+    labels = ['close point']
+    ax1.legend(loc="upper left", bbox_to_anchor=(0.05, 0.95), ncol=3, title=ts_code, shadow=True, fancybox=True, labels=labels)
+    ax1.grid(axis="y")
+
+    ax2 = plt.axes([0.7, 0.3, 0.3, 0.7])
+    ax2.plot(point_data_part["xticks_int_index"], point_data_part["close"], ls="-", lw=1, color='k')
+    ax2.plot(point_data_part["xticks_int_index"], point_data_part["sma_data_5"], ls="-", lw=1, color='r')
+    ax2.plot(point_data_part["xticks_int_index"], point_data_part["sma_data_10"], ls="-", lw=1, color='g')
+    labels = ['close point', 'sma_5', "sma_10"]
+    ax2.legend(loc="upper left", bbox_to_anchor=(0.05, 0.95), ncol=1, title=ts_code, shadow=True, fancybox=True, labels=labels, fontsize='small')
+    ax2.grid(axis="y")
+
+    import matplotlib.ticker as ticker
+    ax2.xaxis.set_major_locator(ticker.MultipleLocator(5))  # 来强制指定每隔5个刻度，设定一个主刻度
+    # 原始数据中日期是不连续的, 反应到x轴的刻度上, x轴会默认补全所有日期, 所以有的日期就会没数据, 这个方法可以去掉没有数据的日期
+    ax2.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+    plt.xticks(rotation=90)
+
+    ax3 = plt.axes([0.7, 0.05, 0.3, 0.1], sharex=ax2)
+    ax3.bar(point_data_part["xticks_int_index"], point_data_part["vol"], width=1)
+    # ax3.bar(point_data_part["trade_date"], point_data_part["vol"], width=np.timedelta64(24, 'h'))  # 如果横坐标是时间的话, width=np.timedelta64(24, 'h') 要使用这种形式
+    ax3.grid(axis="y")
+    plt.xticks(rotation=90)
+
+    plt.show()
+
+
 if __name__ == "__main__":
     point_data = pd.read_csv("../examples/point_data_000001.csv", index_col=[0], parse_dates=[2])
     bs_data = pd.read_csv("../examples/bs_data.csv", index_col=[0], parse_dates=[1])
